44a182f1022e0c1f78fcae7783e51c0828f6988b
Jorge-Rosado <108192150+Jorge-Rosado@users.noreply.github.com>
2023-03-08 00:32:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,64 @@
+//button variables
+let rock = document.querySelector(".rock");
+let paper = document.querySelector(".paper");
+let scissors = document.querySelector(".scissors");
+//display variables
+let displayChoice = document.querySelector(".displayChoice");
+let displayCompChoice = document.querySelector(".displayCompChoice");
+let displayResult = document.querySelector(".result");
+
+
+function computerAnswer(){
+  let number = Math.floor(Math.random() * 3)
+  if(number == 0){
+    displayCompChoice.innerHTML = "The computer chose:Rock"
+    return number
+  }else if(number == 1){
+     displayCompChoice.innerHTML = "The computer chose:Paper"
+    return number
+  }else if(number == 2){
+     displayCompChoice.innerHTML = "The computer chose:Scissors"
+    return number
+  };
+};
+
+function compareAnswers(computerAnswer){
+  if(playerAnswer == computerAnswer){
+    displayResult.innerHTML = "Draw!"
+  }else if(playerAnswer == 0 && computerAnswer == 1){
+    displayResult.innerHTML = "You Lost :("
+  }else if(playerAnswer == 0 && computerAnswer == 2){
+    displayResult.innerHTML = "You Won!"
+  }else if(playerAnswer == 1 && computerAnswer == 0){
+    displayResult.innerHTML = "You Lost :("
+  }else if(playerAnswer == 1 && computerAnswer == 2){
+    displayResult.innerHTML = "You Lost :("
+  }else if(playerAnswer == 2 && computerAnswer == 0){
+    displayResult.innerHTML = "You Lost :("
+  }else if(playerAnswer == 2 && computerAnswer == 1){
+    displayResult.innerHTML = "You Won!"
+  };
+};
+
+let playerAnswer = 10
+//on click events
+rock.onclick = function(){
+  playerAnswer = 0
+  displayChoice.innerHTML = "You Chose:Rock"
+  let number = computerAnswer();
+  compareAnswers(number);
+};
+
+paper.onclick = function(){
+  playerAnswer = 1
+  displayChoice.innerHTML = "You Chose:Paper"
+  let number = computerAnswer();
+  compareAnswers(number);
+};
+
+scissors.onclick = function(){
+  playerAnswer = 2
+  displayChoice.innerHTML = "You Chose:Scissors"
+  let number = computerAnswer();
+  compareAnswers(number);
+};
